75c3575db037d4c230b84c8e44b5c8e3c23744d4
Scott <skibinsc@yahoo.com>
2023-12-15 17:16:24 -0500

    skills page

---
+++ src/assets/language_icons/lang_icons.js
@@ -0,0 +1,23 @@
+import python from "./python.svg";
+import c from "./c.svg";
+import java from "./java.svg";
+import javascript from "./javascript.svg";
+import html from "./html5.svg";
+import css from "./css.svg";
+import mysql from "./mysql.svg";
+import react from "./react.svg";
+import csharp from "./csharp.svg";
+import flutter from "./flutter.svg";
+
+export const icons = [
+  { icon: python, title: "Python", exp: 2 },
+  { icon: c, title: "C", exp: 2 },
+  { icon: csharp, title: "C#", exp: 1 },
+  { icon: java, title: "Java", exp: 1 },
+  { icon: javascript, title: "JS", exp: 0 },
+  { icon: html, title: "HTML", exp: 0 },
+  { icon: css, title: "CSS", exp: 0 },
+  { icon: mysql, title: "MySQL", exp: 0 },
+  { icon: react, title: "React", exp: 0 },
+  { icon: flutter, title: "Flutter", exp: 0 },
+];
--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -1,10 +1,92 @@
 import React from "react";
 import "./Skills.css";
+import { icons } from "../../../assets/language_icons/lang_icons";
 
 export const Skills = () => {
   return (
     <div className="skills">
       <h1>Skills</h1>
+
+      <h2 className="section-title">Coding Languages</h2>
+      <div class="timeline">
+        <div class="container left">
+          <div class="content">
+            <h3> {">"} 2 Years</h3>
+            <div className="content-items-container">
+              {icons
+                .filter((icon) => icon.exp === 2)
+                .map((icon) => (
+                  <div className="content-item">
+                    <img
+                      className="lang-icon"
+                      src={icon.icon}
+                      alt={icon.title}
+                    />
+                    <div className="lang-icon-title">{icon.title}</div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div class="container right">
+          <div class="content">
+            <h3>1 - 2 Years</h3>
+            <div className="content-items-container">
+              {icons
+                .filter((icon) => icon.exp === 1)
+                .map((icon) => (
+                  <div className="content-item">
+                    <img
+                      className="lang-icon"
+                      src={icon.icon}
+                      alt={icon.title}
+                    />
+                    <div className="lang-icon-title">{icon.title}</div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div class="container left">
+          <div class="content">
+            <h3>{"<"} 1 Year</h3>
+            <div className="content-items-container">
+              {icons
+                .filter((icon) => icon.exp === 0)
+                .map((icon) => (
+                  <div className="content-item">
+                    <img
+                      className="lang-icon"
+                      src={icon.icon}
+                      alt={icon.title}
+                    />
+                    <div className="lang-icon-title">{icon.title}</div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <h2 className="section-title">Hard Skills</h2>
+      <h2 className="section-title">Soft Skills</h2>
+      {/* <div className="skills-grid">
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+        <div className="grid-cell">Placeholder</div>
+      </div> */}
     </div>
   );
 };
